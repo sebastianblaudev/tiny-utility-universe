7be8c649e40a750732afb071ad3481d785908561
sebastianblaudev <seba.blau@gmail.com>
2025-05-03 18:06:24 -0400

    fdfd

--- app/products/loading.tsx
+++
@@ -1,7 +0,0 @@
-export default function Loading() {
-  return (
-    <div className="flex items-center justify-center h-screen">
-      <p className="text-muted-foreground">Cargando gestión de productos...</p>
-    </div>
-  )
-}
