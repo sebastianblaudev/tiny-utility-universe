232ea421d5df7ed9d549cd03d9bd2582e16971e7
sebastianblaudev <seba.blau@gmail.com>
2025-07-13 00:06:11 +0200

    dsdsd

--- supabase/migrations/20250708025853-f1c37a89-c52a-41b4-a783-b3bd094a275e.sql
+++
@@ -1,6 +0,0 @@
--- Fix missing tenant_id in sale_items by copying from their parent sales
-UPDATE sale_items 
-SET tenant_id = sales.tenant_id 
-FROM sales 
-WHERE sale_items.sale_id = sales.id 
-AND sale_items.tenant_id IS NULL;
